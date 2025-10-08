1297e80290c13c3882d433a5917118640c729d61
LooseScrews <ftc5237loosescrews2011@gmail.com>
2025-10-08 19:57:04 -0400

    Added mechanum field relative and robot relative drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrivePractice;
+
+public class MecanumFieldOrientatedOpMode extends OpMode {
+
+    MecanumDrivePractice drive = new MecanumDrivePractice();
+
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward, strafe, rotate);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrivePractice.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class MecanumDrivePractice {
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+     private IMU imu;
+
+    public void init(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.get(DcMotor.class, "front_left_motor");
+        backLeftMotor = hwMap.get(DcMotor.class, "back_left_motor");
+        frontRightMotor = hwMap.get(DcMotor.class, "front_right_motor");
+        backRightMotor = hwMap.get(DcMotor.class,"back_right_motor");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        imu = hwMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+    public void drive(double foward, double strafe, double rotate) {
+        double frontLeftPower = foward + strafe + rotate;
+        double backLeftPower = foward - strafe + rotate;
+        double frontRightPower = foward - strafe - rotate;
+        double backRightPower = foward + strafe - rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+        //When going to outreach events w/ robot set maxSpeed lower for a slower robot
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRightMotor.setPower(maxSpeed * (frontRightPower / maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower / maxPower));
+        backLeftMotor.setPower(maxSpeed * (backLeftPower / maxPower));
+
+    }
+
+public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeDegrees(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
+    }
+}
