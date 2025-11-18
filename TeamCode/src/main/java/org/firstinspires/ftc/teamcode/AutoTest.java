aeffbeef85e3582dcf16e69dc0b8d4624549823e
LooseScrews <ftc5237loosescrews2011@gmail.com>
2025-11-18 18:35:23 -0500

    Nov. 15 changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous()
+public class AutoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
+        Action simpleAutoPath = drive.actionBuilder(startPose)
+                .lineToX(48)
+                .turn(Math.toRadians(90))
+                .strafeTo(new Vector2d(48, 12))
+                .lineToX(5)
+                .build();
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        Actions.runBlocking(simpleAutoPath);
+        Pose2d finalPose = drive.pose;
+
+
+        Action parkingPath = drive.actionBuilder(finalPose)
+                .lineToX(0)
+                .build();
+
+        Actions.runBlocking(parkingPath);
+    }
+}
+
