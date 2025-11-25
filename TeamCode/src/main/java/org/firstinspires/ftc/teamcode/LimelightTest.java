0b34f00f7c5b538f604facc7f0975bd4b69583a2
LooseScrews <ftc5237loosescrews2011@gmail.com>
2025-11-25 18:11:37 -0500

    Lime Lite Test
    ran lime light test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
@@ -2,33 +2,50 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
 public class LimelightTest extends OpMode {
 
     private Limelight3A limelight;
+    private IMU imu;
 
     @Override
     public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(8);
-        limelight.start();
+        imu = hardwareMap.get(IMU.class,  "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
 
+
+        limelight.start();
     }
 
     @Override
     public void loop() {
-        LLResult result = limelight.getLatestResult();
-        Pose3D botpose = result.getBotpose();
+        YawPitchRollAngles orientation =imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()){
+            Pose3D botPose = llResult.getBotpose();
+            telemetry.addData( "Target x", llResult.getTx());
+            telemetry.addData("Target y", llResult.getTy());
+            telemetry.addData("Target area", llResult.getTa());
+            telemetry.addData("BotPose", botPose.toString());
+            telemetry.addData("Yaw", botPose.getOrientation().getYaw());
 
-        telemetry.addData("tx", result.getTx());
-        telemetry.addData("ty", result.getTy());
-        telemetry.addData("Botpose", botpose.toString());
+        }
     }
+
+
 
 }
