7a5dda8a065ea0e21e5b03b73c55dd814566f4bb
LooseScrews <ftc5237loosescrews2011@gmail.com>
2025-11-24 20:24:27 -0500

    help red far no fun ):<

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptestin/MeepMeepTestingRedFarAutoThree.java
@@ -0,0 +1,28 @@
+package com.example.meepmeeptestin;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTestingRedFarAutoThree {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 30, 0))
+                .splineToLinearHeading(new Pose2d(35, 30, Math.toRadians(180)), Math.toRadians(0))
+                .waitSeconds(0)
+
+                .build());
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(1.0f)
+                .addEntity(myBot)
+                .start();
+    }
+}
