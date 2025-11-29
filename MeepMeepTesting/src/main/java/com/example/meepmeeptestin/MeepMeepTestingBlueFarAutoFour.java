e34815a531db2475679023a1ac2f157895e1ecd4
LooseScrews <ftc5237loosescrews2011@gmail.com>
2025-11-29 12:13:09 -0500

    Nov. 29th changes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptestin/MeepMeepTestingBlueFarAutoFour.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptestin/MeepMeepTestingBlueFarAutoFour.java
@@ -5,7 +5,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTestingRedFarAutoThree {
+public class MeepMeepTestingBlueFarAutoFour {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -14,23 +14,23 @@ public class MeepMeepTestingRedFarAutoThree {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 30, Math.toRadians(180)))
-                .splineToLinearHeading(new Pose2d(56, 20, Math.toRadians(162)), Math.toRadians(0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -30, Math.toRadians(180)))
+                .splineToLinearHeading(new Pose2d(56, -20, Math.toRadians(192)), Math.toRadians(0))
                 .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(35, 20, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(35, -20, Math.toRadians(-91)), Math.toRadians(0))
                 .waitSeconds(0)
-                .lineToY(50)
-                .splineToLinearHeading(new Pose2d(56, 20, Math.toRadians(162)), Math.toRadians(0))
+                .lineToY(-50)
+                .splineToLinearHeading(new Pose2d(56, -20, Math.toRadians(192)), Math.toRadians(0))
                 .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(40, 60, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(40, -60, Math.toRadians(0)), Math.toRadians(0))
                 .waitSeconds(0)
                 .lineToX(58)
                 .waitSeconds(0)
-                .splineToLinearHeading(new Pose2d(38, 50, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(38, -50, Math.toRadians(0)), Math.toRadians(0))
                 .waitSeconds(0)
-                .splineToLinearHeading(new Pose2d(56, 20, Math.toRadians(162)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(56, -20, Math.toRadians(192)), Math.toRadians(0))
                 .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(40, 20, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(40, -20, Math.toRadians(90)), Math.toRadians(0))
                 .build());
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
                 .setDarkMode(true)
